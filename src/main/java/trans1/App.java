05d34ecee164d55a233617d6376a3340f63832b7
fanghua.tang <fanghua.tang@sff.la>
2017-09-21 10:32:12 +0800

    transction nest

--- src/main/java/trans1/App.java
+++ src/main/java/trans1/App.java
@@ -5,6 +5,7 @@
  */
 package trans1;
 
+import java.util.Arrays;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.context.ApplicationContext;
@@ -22,15 +23,26 @@ public class App {
     public static void main(String[] argv) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         App app = ctx.getBean(App.class);
-        app.test();
+        app.insertTodo();
     }
     @PersistenceContext
     EntityManager em;
 
     @Transactional
-    public void test() {
+    public void insertTodo() {
+        final Tag tag = insertTag("很好");
+        final Tag tag2 = insertTag("Good");
         Todo todo = new Todo();
         todo.title = "测试时放点啥放点啥放点啥";
+        todo.tagList = Arrays.asList(tag, tag2);
         em.persist(todo);
+    }
+
+    @Transactional
+    public Tag insertTag(String name) {
+        Tag tag = new Tag();
+        tag.name = name;
+        em.persist(tag);
+        return tag;
     }
 }
--- src/main/java/trans1/Tag.java
+++ src/main/java/trans1/Tag.java
@@ -10,7 +10,6 @@ import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
@@ -19,18 +18,12 @@ import javax.persistence.ManyToMany;
  * @author sihai
  */
 @Entity
-public class Todo implements Serializable {
-
-    @Id
-    @GeneratedValue
+public class Tag implements Serializable {
+    @Id 
+    @GeneratedValue            
     int id;
-    String title;
-    boolean isFinish;
-
-    @ManyToMany(targetEntity=Tag.class)
-//    @JoinTable( joinColumns = {@JoinColumn(name = "todo_id")}, 
-//            inverseJoinColumns = {@JoinColumn(name = "tag_id")} 
-//    )
-    Collection<Tag> tagList;
-
+    String name;
+    
+    @ManyToMany(mappedBy="tagList" , targetEntity=Todo.class)
+    Collection<Todo> todoList;
 }
